ab19f2eddeab45ac19a6b9e3439ec7e51d30337a
CarlosAndDevs <caguilarcalderon19@gmail.com>
2021-02-03 21:42:24 -0600

    probando jetstream,livewire y tailwind

---
+++ j-livewire-8/resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos cursos</h1>
+        <h2 class="text-xl text-gray-600">Formate como parte de la comunidad de php</h2>
+        <h3 class="text-lg text-gray-600">Somos muchos y php 8 esta bien pro </h3>
+    </div>
+@endsection
