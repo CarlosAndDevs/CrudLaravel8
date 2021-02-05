de35fdc2237c3bb1cea41a80bd6a33d5fa6dd47c
CarlosAndDevs <caguilarcalderon19@gmail.com>
2021-02-04 20:46:03 -0600

    terminando de agregar autenticacion con livewire de laravel 8

--- j-livewire-8/resources/views/index.blade.php
+++ j-livewire-8/resources/views/index.blade.php
@@ -6,4 +6,6 @@
         <h2 class="text-xl text-gray-600">Formate como parte de la comunidad de php</h2>
         <h3 class="text-lg text-gray-600">Somos muchos y php 8 esta bien pro </h3>
     </div>
+
+    <livewire:course-list>
 @endsection
